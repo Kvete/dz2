1a59a162acb6fd664963b421910c93b17901db0e
Anastasya <kvete@icloud.com>
2022-09-21 02:51:03 +0300

    all tasks

---
+++ task1.1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+int number = new Random().Next(10,9999);
+Console.WriteLine($"random number is {number}");
+int res=-1;
+while (number>99)
+{
+    res=number%10;
+    number=number/10;
+}
+if (res>=0)
+{
+Console.WriteLine($"third dijit= {res}");
+}
+else Console.WriteLine("there is no third digit");
---
+++ task1/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int number = new Random().Next(100,1000);
+int res=(number%100-number%10)/10;
+Console.WriteLine($"random number: {number}, second digit from this number: {res}");
